ed999fdebfa6f9f277888be5f66928f60d3b3f1a
sarah Hick <sarah.hick@nventive.com>
2023-08-01 08:44:22 -0400

    fix: docker build step is in one line

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,9 @@
 # build docker images with latest tag and unique tag to have kubernetes detect image change
-docker build -t shicknventive/multi-client:latest \ 
-            -t shicknventive/multi-client:$SHA \ 
-            -f ./client/Dockerfile ./client
+docker build -t shicknventive/multi-client:latest -t shicknventive/multi-client:$SHA -f ./client/Dockerfile ./client
 
-docker build -t shicknventive/multi-server:latest \
-            -t shicknventive/multi-server:$SHA \
-            -f ./server/Dockerfile ./server
+docker build -t shicknventive/multi-server:latest -t shicknventive/multi-server:$SHA -f ./server/Dockerfile ./server
 
-docker build -t shicknventive/multi-worker:latest \
-            -t shicknventive/multi-worker:$SHA \
-            -f ./worker/Dockerfile ./worker
+docker build -t shicknventive/multi-worker:latest -t shicknventive/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # push them to dockerhub
 docker push shicknventive/multi-client:latest
